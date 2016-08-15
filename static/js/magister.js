7f9e3babb64edcfe0cc573ee23eda0c10444f03d
Vickz84259 <vickz84259@gmail.com>
2016-08-15 15:50:47 +0300

    Fixed issue with the local links

--- static/js/magister.js
+++ static/js/magister.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function($) {
 			current_item = this;
 			// close all visible divs with the class of .section
 			$('.section:visible').fadeOut( section_hide_time, function() {
-				$('a', 'mainmenu').removeClass( 'active' );
+				$('a', '.inside').removeClass( 'active' );
 				$(current_item).addClass( 'active' );
 				var new_section = $( $(current_item).attr('href') );
 				new_section.fadeIn( section_show_time );
