0639c23da176cda27673dfa4a48dd42ec76a47d6
Victor Otieno Omondi <vickz84259@gmail.com>
2018-04-12 16:27:51 +0300

    Added twitter api functionality
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- twitter.py
+++ twitter.py
@@ -1,4 +1,6 @@
 import os
+import requests
+import json
 
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
@@ -26,3 +28,42 @@ def get_token():
         token_url=url, client_id=key, client_secret=secret)
 
     return token
+
+
+def get_tweets(query):
+    url = 'https://api.twitter.com/1.1/search/tweets.json'
+    params = {'q': query}
+
+    token = get_token()['access_token']
+    header_value = "Bearer {}".format(token)
+    headers = {'Authorization': header_value}
+
+    response = requests.get(url, params=params, headers=headers)
+    decoded = json.loads(response)
+
+    return decoded['statuses']
+
+
+def get_oembed(tweet_id, username):
+    api_url = 'https://publish.twitter.com/oembed'
+    tweet_url = 'https://twitter.com/{}/statuses/{}'.format(username, tweet_id)
+
+    params = {'url': tweet_url}
+    response = requests.get(api_url, params=params)
+
+    decoded = json.loads(response)
+    return decoded['html']
+
+
+def search(query):
+    tweets = get_tweets(query)
+    result = []
+
+    for tweet in tweets:
+        tweet_id = tweet['id_str']
+        username = tweet['user']['screen_name']
+
+        oembed = get_oembed(tweet_id, username)
+        result.append(oembed)
+
+    return result
